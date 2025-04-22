273dce478dadd17a19ddd7670f41aace9a4bda29
Pedro Augusto <pedroaugustomoura000@gmail.com>
2025-04-22 02:33:54 -0300

    Adicionado dependências na pom.xml + Criado model projeto

---
+++ src/main/java/com/github/projeto/portfolio/model/projeto.java
@@ -0,0 +1,36 @@
+package com.github.projeto.portfolio.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "projetos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class projeto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	
+	private String linkVideo;
+	
+	private String linkDownload;
+	
+	
+}

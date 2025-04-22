b82ec768b260c2adb94a33b036d00cd53aced0b3
Pedro Augusto <pedroaugustomoura000@gmail.com>
2025-04-22 02:13:38 -0300

    Project initializr

---
+++ src/main/java/com/github/projeto/portfolio/SitePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.github.projeto.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/projeto/portfolio/SitePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projeto.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

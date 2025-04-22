566b85fc503c58f7ddf828e8b3037066e9d6f1d7
Pedro Augusto <pedroaugustomoura000@gmail.com>
2025-04-22 03:11:02 -0300

    Criada controller do index junto com o index.html e o estilo básico

--- src/main/java/com/github/projeto/portfolio/model/projeto.java
+++ src/main/java/com/github/projeto/portfolio/model/projeto.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class projeto {
+public class Projeto {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
